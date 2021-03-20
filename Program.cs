3da8623fd4d8f7355d0b0b9a85ff5cc27a1053eb
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-03-20 20:24:10 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            do
+            {
+                Console.WriteLine("Ingrese una frase.");
+                string frase = Console.ReadLine();
+                Console.WriteLine("Para finalizar, debe ingresar la palabra: fin");
+                palabra = Console.ReadLine();
+            } while (palabra!= "fin");
+        }
+    }
+}
